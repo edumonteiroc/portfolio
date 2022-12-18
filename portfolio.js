6bc68a28c458ca91d3ed0cd1f6f496e1876c8daf
Eduardo Monteiro Carvalho <119531326+lokinaice909@users.noreply.github.com>
2022-12-18 20:51:32 -0300

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,16 @@
+/*-----Controle do carrousel----------  */
+$("#nav-item-1").click(function(){
+  $("#carouselExampleControlsNoTouching").carousel(1);
+});
+
+$("#nav-item-2").click(function(){
+  $("#carouselExampleControlsNoTouching").carousel(2);
+});
+
+$("#nav-item-4").click(function(){
+  $("#carouselExampleControlsNoTouching").carousel(3);
+});
+
+$("#nav-item-5").click(function(){
+  $("#carouselExampleControlsNoTouching").carousel(4);
+});
